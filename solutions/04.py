d4f1eba25d2ee167c657e6f4a686225714783409
Patrick Brennan <brennanpatrick20@gmail.com>
2022-12-04 01:03:10 -0500

    Day 4

---
+++ solutions/04.py
@@ -0,0 +1,43 @@
+from aoc.helpers import aoc
+
+# https://adventofcode.com/2022/day/4
+
+@aoc(2022, 4)
+def part1(input):
+    pairs = [p.split(",") for p in input.splitlines()]
+
+    count = 0
+
+    for a, b in pairs:
+        w, x = a.split("-")
+        a_r = range(int(w), int(x) + 1)
+        y, z = b.split("-")
+        b_r = range(int(y), int(z) + 1)
+
+        if len(a_r) > len(b_r):
+            a_r, b_r = b_r, a_r
+
+        if len((set(a_r) & set(b_r))) == len(a_r):
+            count += 1
+    
+    return count
+
+@aoc(2022, 4)
+def part2(input):
+    pairs = [p.split(",") for p in input.splitlines()]
+
+    count = 0
+
+    for a, b in pairs:
+        w, x = a.split("-")
+        a_r = range(int(w), int(x) + 1)
+        y, z = b.split("-")
+        b_r = range(int(y), int(z) + 1)
+
+        if len(a_r) > len(b_r):
+            a_r, b_r = b_r, a_r
+
+        if len((set(a_r) & set(b_r))) > 0:
+            count += 1
+    
+    return count
