0a12e6ab28e671fe623a9c75650f8aecb6a2db53
Patrick Brennan <brennanpatrick20@gmail.com>
2022-12-13 20:21:02 -0500

    Day 13

--- 2022/13.py
+++ 2022/13.py
@@ -1,17 +1,21 @@
 from aoc.helpers import aoc
 
+from pprint import pprint
+
 # https://adventofcode.com/2022/day/13
 
 def run_packet(packet):
 
-    if (isinstance(packet[0], list) and isinstance(packet[1], list)) and len(packet[0]) < len(packet[1]):
-        return False
-    
     match packet:
-            case [[*_], [*_]]:
-                for item in zip(*packet):
-                    if not run_packet(item):
-                        return False
+            case [[*L], [*R]]:
+                for i, item in enumerate(zip(L, R)):
+                    res = run_packet(item)
+                    if res is not None:
+                        return res
+                if len(L) > len(R):
+                    return False
+                elif len(L) == len(R):
+                    return None
             case [[*L], R]:
                 return run_packet((L, [R]))
             case [L, [*R]]:
@@ -19,40 +23,20 @@ def run_packet(packet):
             case [L, R]:
                 if L > R:
                     return False
+                elif L == R:
+                    return None
     return True
 
 
 @aoc(2022, 13)
 def part1(input):
-    input="""[1,1,3,1,1]
-[1,1,5,1,1]
-
-[[1],[2,3,4]]
-[[1],4]
-
-[9]
-[[8,7,6]]
-
-[[4,4],4,4]
-[[4,4],4,4,4]
-
-[7,7,7,7]
-[7,7,7]
-
-[]
-[3]
-
-[[[]]]
-[[]]
-
-[1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
     packets = tuple(tuple(eval(s) for s in p.splitlines()) for p in input.split("\n\n"))
     
     sum = 0
     for i, packet in enumerate(packets):
-        print(run_packet(packet))
+        if run_packet(packet):
+            sum += i + 1
 
     return sum
 
@@ -60,4 +44,20 @@ def part1(input):
 
 @aoc(2022, 13)
 def part2(input):
-    ...
+
+    input += "\n[[2]]\n[[6]]"
+    
+    packets = [eval(p) for p in "\n".join(input.split("\n\n")).splitlines()]
+    
+    for _, packet in list(enumerate(packets)):
+        packets.remove(packet)
+        
+        for x, other_packet in list(enumerate(packets)):
+            if run_packet((packet, other_packet)):
+                packets.insert(x, packet)
+                break
+        else:
+            packets.append(packet)
+
+    return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
+
