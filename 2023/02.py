ce19819033d59e0cb095c15e44375d24a1c7d9cb
Patrick Brennan <brennanpatrick20@gmail.com>
2023-12-02 00:32:22 -0500

    weee midnight code day 2

---
+++ 2023/02.py
@@ -0,0 +1,64 @@
+from aoc.helpers import aoc
+
+# https://adventofcode.com/2023/day/2
+
+
+@aoc(2023, 2)
+def part1(input: str):
+    
+    games = input.strip().splitlines()
+
+    sum = 0
+
+    for game in games:
+        head, sets = game.split(":")
+        id = int(head.split(" ")[-1])
+
+        for thing in sets.split(";"):
+            stuff = {
+                phrase.split(" ")[1]: int(phrase.split(" ")[0])
+                for phrase in thing.strip().split(", ")
+            }
+
+            if (
+                stuff.get("red", 0) > 12
+                or stuff.get("green", 0) > 13
+                or stuff.get("blue", 0) > 14
+            ):
+                break
+        else: # never did I ever
+            sum += id
+    
+    return sum
+
+@aoc(2023, 2)
+def part2(input: str):
+
+    games = input.strip().splitlines()
+
+    sum = 0
+
+    for game in games:
+        head, sets = game.split(":")
+        id = int(head.split(" ")[-1])
+
+        red = 0
+        blue = 0
+        green = 0
+
+        for thing in sets.split(";"):
+            stuff = {
+                phrase.split(" ")[1]: int(phrase.split(" ")[0])
+                for phrase in thing.strip().split(", ")
+            }
+
+            if stuff.get("red") is not None and stuff.get("red") > red:
+                red = stuff["red"]
+            if stuff.get("green") is not None and stuff.get("green") > green:
+                green = stuff["green"]
+            if stuff.get("blue") is not None and stuff.get("blue") > blue:
+                blue = stuff["blue"]
+
+        sum += red * blue * green
+    
+    return sum
